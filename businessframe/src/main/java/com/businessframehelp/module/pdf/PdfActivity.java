6a0789c0d6e97ead4993170553092f7dcfd86291
DESKTOP-VJC6IHR\King6rf <3521829873@qq.com>
2017-07-17 17:04:37 +0800

    我没空

--- businessframe/src/main/java/com/businessframehelp/module/pdf/PdfActivity.java
+++ businessframe/src/main/java/com/businessframehelp/module/pdf/PdfActivity.java
@@ -1,12 +1,17 @@
 package com.businessframehelp.module.pdf;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.businessframehelp.R;
 import com.businessframehelp.utils.BarUtil;
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.scroll.ScrollHandle;
+
+import java.io.File;
 
 
 /**
@@ -14,12 +19,61 @@ import com.businessframehelp.utils.BarUtil;
  */
 
 public class PdfActivity extends AppCompatActivity {
+
+    private PDFView pdfView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pdf_layout);
         BarUtil.initBar(this);
+        pdfView = (PDFView) findViewById(R.id.pdfview);
+        pdfView.fromFile(new File(Environment.getExternalStorageDirectory()+"/test2017.pdf"))
+                .swipeHorizontal(true)
+                .scrollHandle(new ScrollHandle() {
+                    @Override
+                    public void setScroll(float position) {
 
+                    }
+
+                    @Override
+                    public void setupLayout(PDFView pdfView) {
+
+                    }
+
+                    @Override
+                    public void destroyLayout() {
+
+                    }
+
+                    @Override
+                    public void setPageNum(int pageNum) {
+
+                    }
+
+                    @Override
+                    public boolean shown() {
+                        return true;
+                    }
+
+                    @Override
+                    public void show() {
+
+                    }
+
+                    @Override
+                    public void hide() {
+
+                    }
+
+                    @Override
+                    public void hideDelayed() {
+                        if(pdfView.getZoom()==1){
+                            pdfView.jumpTo(pdfView.getCurrentPage());
+                        }
+                    }
+                })
+                .load();
     }
 
     @Override
@@ -28,7 +82,8 @@ public class PdfActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_pdf, menu);
         return true;
     }
-
+    public void lookTask(){}
+    public void editTask(){}
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -38,16 +93,13 @@ public class PdfActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_pdflook) {
+            lookTask();
             return true;
         }
         if (id == R.id.action_pdfedit) {
+            editTask();
             return true;
         }
-        if (item.getItemId() == android.R.id.home){
-            finish();
-            return true ;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
--- helppdf/src/main/java/com/wisdomregulation/pdflink/IPdf_BackFrame.java
+++
@@ -1,8 +0,0 @@
-package com.wisdomregulation.pdflink;
-
-/**
- * Created by King6rf on 2017/2/17.
- */
-public interface IPdf_BackFrame {
-    public void writeContent () throws Exception;
-}
