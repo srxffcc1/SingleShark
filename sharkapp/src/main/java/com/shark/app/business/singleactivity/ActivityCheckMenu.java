e2f6df3434496befd22d2d6bd87ff104970d3004
DESKTOP-VJC6IHR\King6rf <3521829873@qq.com>
2018-01-12 16:56:00 +0800

    上传

--- sharkapp/src/main/java/com/shark/app/business/singleactivity/ActivityCheckMenu.java
+++ sharkapp/src/main/java/com/shark/app/business/singleactivity/ActivityCheckMenu.java
@@ -1,9 +1,12 @@
 package com.shark.app.business.singleactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.businessframehelp.app.FrameActivity;
 import com.businessframehelp.enums.ORIENTATION;
@@ -21,6 +24,15 @@ public class ActivityCheckMenu extends FrameActivity {
     private ArrayList<Fragment> mFragments = new ArrayList<>();
     private SegmentTabLayout mTabLayout_3;
     int testlengtht=17;
+    public LinearLayout jihual;
+    public LinearLayout fanganl;
+    public LinearLayout xianchangl;
+    public LinearLayout cuoshil;
+    public LinearLayout zelingl;
+    public LinearLayout fuchal;
+    public LinearLayout chufal;
+    public LinearLayout guidangl;
+
     @Override
     public ORIENTATION getORIENTATION() {
         return null;
@@ -45,5 +57,19 @@ public class ActivityCheckMenu extends FrameActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkmenu);
+        jihual= (LinearLayout) findViewById(R.id.jihual);
+        fanganl= (LinearLayout) findViewById(R.id.fanganl);
+        xianchangl= (LinearLayout) findViewById(R.id.xianchangl);
+        xianchangl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),ActivityXianChangJianCha.class));
+            }
+        });
+        cuoshil= (LinearLayout) findViewById(R.id.cuoshil);
+        zelingl= (LinearLayout) findViewById(R.id.zelingl);
+        fuchal= (LinearLayout) findViewById(R.id.fuchal);
+        chufal= (LinearLayout) findViewById(R.id.chufal);
+        guidangl= (LinearLayout) findViewById(R.id.guidangl);
     }
 }
